f3c6ae4bcb839720bd749c3f09b1e5bd58aebdf0
aptonic <contact@aptonic.com>
2013-10-28 23:48:10 +1300

    Updated site

--- generator/source/support.php
+++ generator/source/support.php
@@ -9,25 +9,7 @@
 			<h2>Contact Us</h2>
 			<a href="mailto:frenzyapp@aptonic.com">General/Support</a><br>
 			<a href="mailto:frenzybugs@aptonic.com">Report Bug</a><br>
-			<a href="http://twitter.com/johnwinter">Twitter</a><br><br>
-
-			<h2>Questions and Common Issues</h2>
-
-			<h3>I just purchased Frenzy but I haven't received my license key.</h3> 
-		    <p>Check your spam folder. You should normally receive your license details within a minute.<br>If you still haven't received your license key after a few hours <a href="mailto:support@aptonic.com">drop us an email.</a></p> 
-
-		    <h3>I lost my license key.</h3>	
-			<p>Put your email address in on <a href="http://frenzyapp.com/payments/LostSerial.php">this page</a> and we'll send it to you again.</p> 
-
-			<h3>My license key isn't working.</h3>	
-			<p>Try disabling Little Snitch or any other firewall software that you have running and try again.<br />If you're still stuck, <a href="mailto:support@aptonic.com">email us.</a></p> 
-
-			<h3>Can I use my license key to register Frenzy on multiple Macs?</h3> 
-			<p>If you are the user of the license that's fine.</p> 
-
-			<h3>What's your upgrade policy?</h3> 
-			<p>Point releases are free updates. Major releases e.g. 2.0, 3.0 will be paid upgrades.</p> 
-
+			<a href="http://twitter.com/aptonic">Twitter</a><br><br>
 		</div>
 		
 		<div class="divider"></div>
