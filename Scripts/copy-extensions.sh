0eefd6edb7d340e9bbbb7f8fa2a82842c643bd17
aptonic <contact@aptonic.com>
2013-11-01 15:09:30 +1300

    Fixed issue with hard coded path in copy-extensions.sh (Closes #3) - thanks @pryley

--- Scripts/copy-extensions.sh
+++ Scripts/copy-extensions.sh
@@ -1,7 +1,7 @@
 set -o errexit
 echo "Copying browser extensions to app bundle Extensions directory"
 
-EXTENSIONS_PATH="/Users/john/Documents/XcodeProjects/Frenzy/Extensions"
+EXTENSIONS_PATH="$SRCROOT/Extensions"
 EXTENSIONS_DIR="$BUILT_PRODUCTS_DIR/$PROJECT_NAME.app/Contents/Extensions"
 
 rm -rf "$EXTENSIONS_DIR"
